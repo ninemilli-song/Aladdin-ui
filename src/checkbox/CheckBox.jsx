dab07a67daa45e579fcc67c9b728c8da0965500e
ninemilli.song <ninemilli_song@163.com>
2019-06-24 15:46:04 +0800

    test husky

--- src/checkbox/CheckBox.jsx
+++ src/checkbox/CheckBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const CheckBox = (props) => {
+    const { a } = props;
     return (
-        <div className='aladdin-checkbox'>
+        <div className="aladdin-checkbox">
             Hello CheckBox!
+            {a}
         </div>
-    )
+    );
 };
 
 export default CheckBox;
