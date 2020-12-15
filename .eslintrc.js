d35281ef29dfb71a6ac623d4f10e2f9579860035
ninemilli.song <ninemilli_song@163.com>
2020-12-15 14:15:30 +0800

    feat: add checkbox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
     extends: [require.resolve('@umijs/fabric/dist/eslint')],
+    rules: {
+        'react/require-default-props': 0,
+        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
+    },
 };
---
+++ components/checkbox/CheckBox.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { CheckBoxProps, KindMap } from './interface';
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const prefixCls = 'aladdin-checkbox';
+
+const CheckBox: React.FC<CheckBoxProps> = (props) => {
+  const { kind } = props;
+  return (
+    <div
+      className={prefixCls}
+      style={{
+        background: kinds[kind],
+      }}
+    >
+      Hello CheckBox!
+      {info}
+    </div>
+  );
+};
+
+CheckBox.propTypes = {
+  kind: PropTypes.oneOf(['info', 'positive', 'negative', 'warning']),
+};
+
+CheckBox.defaultProps = {
+  kind: 'info',
+};
+
+export default CheckBox;
